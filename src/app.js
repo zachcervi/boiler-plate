aabdac4d4ef361a038ae78bb39b72e1b1fe6c236
Zach Cervi <zachcervi@gmail.com>
2022-01-26 08:55:32 -0700

    fix styles and import

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "normalize.css/normalize.css";
-import "../src/styles/styles/styles.scss";
+import "../src/styles/styles.scss";
 
 ReactDOM.render(<p>Boilerplate</p>, document.getElementById("app"));
